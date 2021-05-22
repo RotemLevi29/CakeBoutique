14a3ec3a1c1e6a9f3a20d22bcfd7e76d2b7ac8af
inon47 <72727657+inon47@users.noreply.github.com>
2021-05-22 19:13:40 +0300

    reset

--- jewelry/Models/Address.cs
+++ jewelry/Models/Address.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace jewelry.Models
 {
-    //blah 
     public class Address
     {
-        //master changes
         public int  Id { get; set; }
 
         [DataType(DataType.PhoneNumber)]
