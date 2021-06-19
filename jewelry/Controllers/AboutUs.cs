05912d303df0166bac36b11e3a2a9c2913d15422
or <or@DESKTOP-RLS14BH>
2021-06-19 18:09:12 +0300

    nadav

---
+++ jewelry/Controllers/AboutUs.cs
@@ -0,0 +1,26 @@
+﻿using jewelry.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace jewelry.Controllers
+{
+    public class AboutUs : Controller
+    {
+
+        private readonly jewelryContext _context;
+
+        public AboutUs(jewelryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
